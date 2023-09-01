a2d10168f556e5a8162cfd1360941d48e868480e
resetmerlin <resetmerlin@gmail.com>
2023-09-01 18:46:07 +0900

    feat: About Object 생생 후 Source, Item, Group, Section property 추가

---
+++ client/src/components/about/About.ts
@@ -0,0 +1,11 @@
+import AboutGroup from './AboutGroup'
+import AboutItem from './AboutItem'
+import AboutSection from './AboutSection'
+import AboutSource from './AboutSource'
+
+export const About = {
+    Source: AboutSource,
+    Item: AboutItem,
+    Group: AboutGroup,
+    Section: AboutSection,
+}
