6e54106a48e39739bd4fa1bdad9f10b0380c17c3
resetmerlin <resetmerlin@gmail.com>
2023-09-01 18:40:44 +0900

    feat: html element props define해줄 type 파일 추가

---
+++ client/src/types/element-props.ts
@@ -0,0 +1,26 @@
+import {
+    ButtonHTMLAttributes,
+    DetailedHTMLProps,
+    HTMLAttributes,
+    InputHTMLAttributes,
+} from 'react'
+
+export type TNormalElementProps<Element> = DetailedHTMLProps<
+    HTMLAttributes<Element>,
+    Element
+>
+
+export type TDivElementProps<Element = HTMLDivElement> = DetailedHTMLProps<
+    HTMLAttributes<Element>,
+    Element
+>
+
+export type TInputElementProps<Element = HTMLInputElement> = DetailedHTMLProps<
+    InputHTMLAttributes<Element>,
+    Element
+>
+
+export type TButtonElementProps = DetailedHTMLProps<
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+>
