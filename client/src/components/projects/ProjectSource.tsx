7f142fb9d5a8e78bf2a59010ee9809ef870eb475
resetmerlin <resetmerlin@gmail.com>
2023-09-01 18:49:34 +0900

    feat: Project Source component 추가

---
+++ client/src/components/projects/ProjectSource.tsx
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom'
+import './Project.css'
+
+interface IProps {
+    github: URL | string
+    obsidian: URL | string
+}
+
+export default function ProjectSource({ github, obsidian }: IProps) {
+    return (
+        <div className="mt-2 flex">
+            <Link to={github} target="_blank">
+                <i className="bx bxl-github text-5xl " />
+            </Link>
+
+            <Link to={obsidian} target="_blank">
+                <img
+                    src="./obsidian-icon.png"
+                    alt="icon"
+                    className="h-14 w-14"
+                />
+            </Link>
+        </div>
+    )
+}
