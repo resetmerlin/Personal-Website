df4072278a966dabff99a1da2fde40d8d3d08f0b
resetmerlin <resetmerlin@gmail.com>
2023-09-01 18:50:14 +0900

    feat: Project Object 생생 후 Source, Item, Group, Section property 추가

---
+++ client/src/components/projects/Project.ts
@@ -0,0 +1,11 @@
+import ProjectGroup from './ProjectGroup'
+import ProjectItem from './ProjectItem'
+import ProjectSection from './ProjectSection'
+import ProjectSource from './ProjectSource'
+
+export const Project = {
+    Source: ProjectSource,
+    Item: ProjectItem,
+    Group: ProjectGroup,
+    Section: ProjectSection,
+}
