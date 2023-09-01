7e1ea8e30fe0dde85bb02b2aac7f19013f7a9ae0
resetmerlin <resetmerlin@gmail.com>
2023-09-01 17:18:21 +0900

    feat: Laptop UI component 추가

---
+++ client/src/components/laptop/Laptop.tsx
@@ -0,0 +1,15 @@
+import './Laptop.css'
+
+export default function Laptop() {
+    return (
+        <div className="laptop w-4/5">
+            <div className="laptop__screen">
+                <img
+                    src="./nikesnrks.png "
+                    alt="nikeSnrks"
+                    className="h-full w-full"
+                />
+            </div>
+        </div>
+    )
+}
