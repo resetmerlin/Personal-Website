73bf4694e571ec22554f5f49d9825c1d21afa4aa
resetmerlin <resetmerlin@gmail.com>
2023-09-01 18:54:11 +0900

    feat: About Page component 추가

---
+++ client/src/page/about/AboutPage.tsx
@@ -0,0 +1,60 @@
+import IntroNav from '../../components/introNav/IntroNav'
+import { Layout } from '../../components/layouts'
+import { About } from '../../components/about'
+import { Three } from '../../components/three'
+import { Fog } from '../../components/three/parts/fog'
+import { Lights } from '../../components/three/parts'
+import { Earth } from '../../components/three/object/earth'
+import { Subtitle, Title } from '../../components/writing'
+
+export default function AboutPage() {
+    return (
+        <Layout>
+            <About.Section>
+                <About.Group id="project-left" className="max-[500px]:hidden">
+                    <div
+                        className="relative flex justify-center rounded-full max-[500px]:translate-x-[-19rem] max-[500px]:translate-y-[6rem]	sm:translate-x-[-19rem] sm:translate-y-[6rem]	 md:translate-x-[-13rem] md:translate-y-[6rem] lg:translate-x-[-8rem] xl:translate-x-[-7rem] 2xl:translate-x-0  2xl:translate-y-0"
+                        id="earth-project"
+                    >
+                        <IntroNav
+                            page="ABOUT"
+                            id="about__title"
+                            className="sm:translate-x-[-16rem] sm:translate-y-[-6rem] "
+                        />
+                        <div className="absolute" id="shadow" />
+                        <Three.Container>
+                            <Fog args={['black', 0, 60]} />
+                            <Lights.Ambient />
+                            <Lights.Directional />
+                            <Earth.Group>
+                                <Earth.Mesh />
+                            </Earth.Group>
+                        </Three.Container>
+                    </div>
+                </About.Group>
+                <About.Group className="max-[500px]:w-full">
+                    <About.Item className="flex-col ">
+                        <img
+                            src="./profile.png"
+                            alt="profile"
+                            className="h-96	w-96 rounded-full border-8	border-neutral-700	 p-8"
+                            id="about__profile"
+                        />
+                        <div className="flex w-4/6 flex-col justify-center 	">
+                            <Title className="sm:text-7xl	md:text-8xl">
+                                Hi my name is Merlin
+                            </Title>
+                            <Subtitle>
+                                I'm looking for a job related to frontend
+                                development.
+                                <br /> I also have an interest in learning
+                                methodology <br /> so that I can improve my
+                                development skills.
+                            </Subtitle>
+                        </div>
+                    </About.Item>
+                </About.Group>
+            </About.Section>
+        </Layout>
+    )
+}
